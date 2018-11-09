f3cadfa7d24414ad4cd3aaba552c3e8656565dbf
AzureAD\DungTruong <dung.truong@acomsolutions.com>
2018-11-09 11:42:17 +0700

    cm

---
+++ src/exercise/V_III_Flash.java
@@ -0,0 +1,35 @@
+package exercise;
+/* Author: truonganhdung
+ * Created Date: 11/09/2018
+ * Modified Date: ../../2018
+ * https://www.guru99.com/flash-testing-selenium.html
+ * */
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import Flash.FlashObjectWebDriver;
+
+public class V_III_Flash {
+	@Test
+	public void main() throws InterruptedException {
+		FirefoxDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		
+		FlashObjectWebDriver flashApp = new FlashObjectWebDriver(driver, "myFlashMovie");
+		
+		driver.get("http://demo.guru99.com/test/flash-testing.html");
+		
+		Thread.sleep(2000);
+		flashApp.callFlashObject("Play");
+		
+		Thread.sleep(2000);
+		flashApp.callFlashObject("StopPlay");
+		
+		Thread.sleep(2000);
+		flashApp.callFlashObject("SetVariable", "/:message", "Flash testing using selenium Webdriver");
+		
+		System.out.println(flashApp.callFlashObject("GetVariable", "/:message"));
+		
+		driver.close();
+	}
+}
